3d61b2c019eef099f5f47e53e569a146a9afac48
yhyzgn <yhyzgn@gmail.com>
2019-10-31 14:13:05 +0800

    format

--- stylus.go
+++ stylus.go
@@ -23,54 +23,34 @@ type Stylus int
 
 // 字体颜色
 const (
-	// 黑色
-	FontBlack Stylus = 30
-	// 红色
-	FontRed Stylus = 31
-	// 绿色
-	FontGreen Stylus = 32
-	// 黄色
-	FontYellow Stylus = 33
-	// 蓝色
-	FontBlue Stylus = 34
-	// 紫色
-	FontPurple Stylus = 35
-	// 蓝绿
-	FontBlueGreen Stylus = 36
-	// 白色
-	FontWhite Stylus = 37
+	FontBlack     Stylus = 30 // 黑色
+	FontRed                   // 红色
+	FontGreen                 // 绿色
+	FontYellow                // 黄色
+	FontBlue                  // 蓝色
+	FontPurple                // 紫色
+	FontBlueGreen             // 蓝绿
+	FontWhite                 // 白色
 )
 
 // 背景颜色
 const (
-	// 黑色
-	BackBlack Stylus = 40
-	// 红色
-	BackRed Stylus = 41
-	// 绿色
-	BackGreen Stylus = 42
-	// 黄色
-	BackYellow Stylus = 43
-	// 蓝色
-	BackBlue Stylus = 44
-	// 紫色
-	BackPurple Stylus = 45
-	// 蓝绿
-	BackBlueGreen Stylus = 46
-	// 白色
-	BackWhite Stylus = 47
+	BackBlack     Stylus = 40 // 黑色
+	BackRed                   // 红色
+	BackGreen                 // 绿色
+	BackYellow                // 黄色
+	BackBlue                  // 蓝色
+	BackPurple                // 紫色
+	BackBlueGreen             // 蓝绿
+	BackWhite                 // 白色
 )
 
 // 字体风格
 const (
-	// 粗体
-	StyleBold Stylus = 1
-	// 斜体
-	StyleItalic Stylus = 3
-	// 下划线
-	StyleUnderLine Stylus = 4
-	// 反转
-	StyleReverse Stylus = 7
+	StyleBold      Stylus = 1 // 粗体
+	StyleItalic    Stylus = 3 // 斜体
+	StyleUnderLine Stylus = 4 // 下划线
+	StyleReverse   Stylus = 7 // 反转
 )
 
 // 给任何将要输出的对象加上颜色风格
